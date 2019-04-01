587abcc1fe2524c5ef781c2217a0a5e786b2d4de
mixxamm <maximjanssensj5@gmail.com>
2019-04-01 12:34:40 +0200

    Maak optie om af te vinken
    Indien je zeker bent dat een optie onmogelijk is, kan je deze afvinken en telt die niet meer mee.

--- MeerkeuzeVraagKiezer/MainPage.xaml.cs
+++ MeerkeuzeVraagKiezer/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -31,7 +32,79 @@ namespace MeerkeuzeVraagKiezer
 
         private void ButtonGooi_Click(object sender, RoutedEventArgs e)
         {
-            textBlockResultaat.Text = ((Keuze)random.Next(0, int.Parse(textBoxAantalKeuzes.Text))).ToString();
+            bool goOn;
+            string letter = "";
+            do
+            {
+                goOn = false;
+                letter = ((Keuze)random.Next(0, int.Parse(textBoxAantalKeuzes.Text))).ToString();
+
+                foreach (Control control in stackPanelCheckBoxes.Children)
+                {
+                    if (control is CheckBox)
+                    {
+                        CheckBox checkBox = (CheckBox)control;
+                        if(!(bool)checkBox.IsChecked && letter == (string)checkBox.Content)
+                            goOn = true;
+                    }
+                }
+            } while (goOn);
+            textBlockResultaat.Text = letter;
+        }
+
+        private void TextBoxAantalKeuzes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(textBoxAantalKeuzes.Text, out int result))
+            {
+                Visibility visibility = Visibility.Visible;
+                switch (result)
+                {
+                    case 1:
+                        checkBoxA.Visibility = visibility;
+                        break;
+                    case 2:
+                        checkBoxB.Visibility = visibility;
+                        goto case 1;
+                    case 3:
+                        checkBoxC.Visibility = visibility;
+                        goto case 2;
+                    case 4:
+                        checkBoxD.Visibility = visibility;
+                        goto case 3;
+                    case 5:
+                        checkBoxE.Visibility = visibility;
+                        goto case 4;
+                    case 6:
+                        checkBoxF.Visibility = visibility;
+                        goto case 5;
+                    case 7:
+                        checkBoxG.Visibility = visibility;
+                        goto case 6;
+                    case 8:
+                        checkBoxH.Visibility = visibility;
+                        goto case 7;
+                    case 9:
+                        checkBoxI.Visibility = visibility;
+                        goto case 8;
+                    case 10:
+                        checkBoxJ.Visibility = visibility;
+                        goto case 9;
+                    case 11:
+                        checkBoxK.Visibility = visibility;
+                        goto case 10;
+                    default:
+                        visibility = Visibility.Collapsed;
+                        goto case 11;
+                }
+            }
+            else
+            {
+                foreach(Control control in stackPanelCheckBoxes.Children)
+                {
+                    if (control is CheckBox)
+                        control.Visibility = Visibility.Collapsed;
+                }
+            }
         }
     }
 }
